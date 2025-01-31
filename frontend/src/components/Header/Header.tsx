ed09d4347b575a08c18d5a2a3a4ee1dd7c8441ed
noninsane <nattanon3112@gmail.com>
2025-01-31 20:05:24 +0700

    Header structure(unfinished)

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="logo-image">
+            <img src="https://images.pexels.com/photos/2558605/pexels-photo-2558605.jpeg" alt="" />
+        </div>
+        <nav>
+            <a href="#">หน้าแรก</a>
+            <a href="#">คุณสมบัติ</a>
+            <a href="#">ราคา</a>
+            <a href="#">สั่งซื้อ</a>
+            <a href="#">ติดต่อเรา</a>
+        </nav>
+        <div className="register-btn">    
+            <button>เข้าใช้งาน</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
